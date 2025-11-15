7a5a37db22171a93dbb1dc9acde5cfeb3b721fc2
picupiee <picupiee.id@gmail.com>
2025-11-15 18:51:08 +0700

    adding new permission

--- config/permissions.ts
+++ config/permissions.ts
@@ -9,6 +9,11 @@ export const PERMISSIONS = {
   UPDATE_RECORD: "update:record",
   DELETE_RECORD: "delete:record",
   VIEW_SENSITIVE_NAME: "view:sensitive_name",
+  // EXPORT_RECORD: "export:record"
+  CREATE_ACTIVITY: "create:activity",
+  VIEW_ACTIVITY_DETAIL: "view:activity:detail",
+  EDIT_ACTIVITY: "edit:activity",
+  DELETE_ACTIVITY: "delete:activity",
 };
 
 // 2. Define the Access Matrix (The core RBAC logic)
@@ -20,6 +25,10 @@ export const ACCESS_MATRIX: Record<UserRole, string[]> = {
     PERMISSIONS.UPDATE_RECORD,
     PERMISSIONS.DELETE_RECORD,
     PERMISSIONS.VIEW_SENSITIVE_NAME,
+    PERMISSIONS.CREATE_ACTIVITY,
+    PERMISSIONS.VIEW_ACTIVITY_DETAIL,
+    PERMISSIONS.EDIT_ACTIVITY,
+    PERMISSIONS.DELETE_ACTIVITY,
   ],
 
   // Can view, create, and update, but NOT delete
@@ -28,6 +37,9 @@ export const ACCESS_MATRIX: Record<UserRole, string[]> = {
     PERMISSIONS.CREATE_RECORD,
     PERMISSIONS.UPDATE_RECORD,
     PERMISSIONS.VIEW_SENSITIVE_NAME,
+    PERMISSIONS.CREATE_ACTIVITY,
+    PERMISSIONS.VIEW_ACTIVITY_DETAIL,
+    PERMISSIONS.EDIT_ACTIVITY,
   ],
 
   // Can only view
