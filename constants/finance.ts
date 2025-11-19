825e7f5766c95370877e88986597ee16d928dd00
picupiee <picupiee.id@gmail.com>
2025-11-19 20:40:54 +0700

    Creating finance constant file

---
+++ constants/finance.ts
@@ -0,0 +1,46 @@
+export const FINANCE_INCOME_SOURCES = [
+  {
+    id: "disposal_fee",
+    label: "Iuran Kebersihan",
+    note: "Iuran bulanan layanan sampah",
+  },
+  { id: "rt_fee", label: "Iuran Kas RT", note: "Iuran bulanan kas lingkungan" },
+  {
+    id: "donation_general",
+    label: "Donasi Umum",
+    note: "Sumbangan non-terikat",
+  },
+  {
+    id: "other_income",
+    label: "Lain-lain",
+    note: "Sumber Penerimaan non-rutin (Rincikan sumber dananya)",
+  },
+];
+
+export const FINANCE_SPENDING_TYPES = [
+  {
+    id: "aid_personal",
+    label: "Bantuan Sosial (Umum)",
+    note: "Dana Bantuan Sosial (Sakit Keras / Kecelakaan dan/atau Duka Cita)",
+  },
+  {
+    id: "aid_community",
+    label: "Bantuan Komunitas",
+    note: "Dana Bantuan Perbaikan Fasilitas Umum",
+  },
+  {
+    id: "maintenance_general",
+    label: "Biaya Operasional",
+    note: "ATK, Listrik, Kebutuhan Umum Lainnya, dll. (Mohon Dirincikan!)",
+  },
+  {
+    id: "event_budget",
+    label: "Anggaran Kegiatan",
+    note: "Biaya pelaksanaan acara RT.",
+  },
+  {
+    id: "disposal_payment",
+    label: "Pembayaran Jasa Sampah",
+    note: "Gaji/bayar jasa petugas kebersihan.",
+  },
+];
